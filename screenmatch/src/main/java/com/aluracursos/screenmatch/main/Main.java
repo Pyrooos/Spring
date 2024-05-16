3e600da45559f9be18df98d2a93ca601206a6973
Pyrooos <cris@MacBook-Air-de-Cristian.local>
2024-05-16 17:13:21 -0600

    start final challenges

--- screenmatch/src/main/java/com/aluracursos/screenmatch/main/Main.java
+++ screenmatch/src/main/java/com/aluracursos/screenmatch/main/Main.java
@@ -10,10 +10,7 @@ import com.aluracursos.screenmatch.service.toConvertData;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Main {
@@ -78,13 +75,13 @@ public class Main {
         //episodes.forEach(System.out::println);
 
         //search episodes starting from the year
-        System.out.println("Please write the year of the series you want ");
+        /*System.out.println("Please write the year of the series you want ");
         var date = keyboard.nextInt();
-        keyboard.nextLine();
+        keyboard.nextLine();*/
 
-        LocalDate dateSearch = LocalDate.of(date, 1, 1);
+       /* LocalDate dateSearch = LocalDate.of(date, 1, 1);
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-
+*/
 //        episodes.stream()
 //                .filter(e ->e.getDateRelease() != null && e.getDateRelease().isAfter(dateSearch))
 //                .forEach(e -> System.out.println(
@@ -94,10 +91,29 @@ public class Main {
 //                                " Release date: "+ e.getDateRelease().format(dtf)
 //                ));
         // search a part of the title on an episode
-        System.out.println("Please write a part of the title you want search");
+        /*System.out.println("Please write a part of the title you want search");
          var partOfTitle =keyboard.nextLine();
-         episodes.stream()
-                 .filter(e-> e.getTitle().contains(partOfTitle))
-                 .findFirst();
+        Optional<Episode> episodeSearch = episodes.stream()
+                .filter(e -> e.getTitle().toUpperCase().contains(partOfTitle.toUpperCase()))
+                .findFirst();
+        if (episodeSearch.isPresent()){
+            System.out.println("Episode found");
+            System.out.println("The data is: "+ episodeSearch.get());
+        } else {
+            System.out.println("Episode not found");
+        }*/
+        Map<Integer, Double> ratingsPerSeason = episodes.stream()
+                .filter(e-> e.getRating() > 0.0)
+                .collect(Collectors.groupingBy(Episode::getSeason,
+                        Collectors.averagingDouble(Episode::getRating)));
+        System.out.println(ratingsPerSeason);
+
+        DoubleSummaryStatistics est = episodes.stream()
+                .filter(e-> e.getRating() > 0.0)
+                .collect(Collectors.summarizingDouble(Episode::getRating));
+        System.out.println(est);
+        System.out.println("Media of the ratings:"+est.getAverage());
+        System.out.println("The Best episode rated:"+ est.getMax());
+        System.out.println("The worst episode rated"+est.getMin());
     }
 }
